e4a26a955b422095c0b54962cff953f1136de6aa
mistake2000-Git <18521002@gm.uit.edu.vn>
2021-11-06 22:26:20 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const URL = 'mongodb+srv://admin:cRH5G-9%21%23AX8c9%24@webapp.mfwqf.mongodb.net/test?authSource=admin&replicaSet=atlas-angb4h-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+const URL = 'mongodb+srv://admin:cRH5G-9%21%23AX8c9%24@webapp.mfwqf.mongodb.net/RubikShop?authSource=admin&replicaSet=atlas-angb4h-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
 const app = express()
-
+const PORT = process.env.PORT||3000;
 //Connect MongoDB
 const connectDB = async () => {
   try {
@@ -13,7 +13,7 @@ const connectDB = async () => {
         useUnifiedTopology: true
       }
     )
-
+    app.listen(PORT,()=>console.log("Server is started at 3000 PORT"))
     console.log('Connected to mongoDB')
   } catch (error) {
     console.log(error)
@@ -22,8 +22,8 @@ const connectDB = async () => {
 }
 connectDB()
 
-const PORT = process.env.PORT||3000;
-app.listen(3000,()=>console.log("Server is started at 3000 PORT"))
+
+
 
 const cubeRouter = require(__dirname+'/routes/cube')
 app.use('/cube',cubeRouter)
